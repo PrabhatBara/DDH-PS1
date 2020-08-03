862a53296e5d4ed2e01aee3b78ce0ad169e1acb3
arun prasad <n.arun1092@gmail.com>
2020-08-04 00:47:59 +0530

    first commit

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ from zinc_id.get_zincid import get_zincid_from_smile
 
 zinc_id = get_zincid_from_smile("ClC1=CC(NC(=O)CSC2=NC=CC(=N2)C2=CSC(=N2)C2=CC=CC=C2)=CC(Cl)=C1")
 print(zinc_id)
+
+print("arun")
